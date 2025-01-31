bdbd7492d3f905965ad09056b5dc0e37adaa05b0
vmelero <vmelero@hotmail.com>
2025-01-31 13:02:34 +0100

    Sobreescribiendo proyecto con nueva version

---
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-page',
+  templateUrl: './hero-page.component.html',
+  styles: [
+  ]
+})
+export class HeroPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+
+
+
+@NgModule({
+  
+  exports: [   
+  ]
+})
+export class MaterialModule { }
